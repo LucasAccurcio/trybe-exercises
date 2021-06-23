810e07a45fcd3d0e712787da5bb348bd0720c126
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-06-23 18:32:47 -0300

    Exercício 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise1.js
@@ -0,0 +1,17 @@
+let a = 2;
+let b = 1;
+
+//Soma
+console.log(a+b);
+
+//Subtração
+console.log(a-b);
+
+//Multiplicação
+console.log(a*b);
+
+//Divisão
+console.log(a/b);
+
+//Módulo ou resta
+console.log(a%b);
