6885d57f6be05d006b4fb0ed6d119d2fd5613812
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-06-28 19:20:40 -0300

    Exercícios 2 e 3 da Parte II Funções

--- exercises/bloco_4/dia_4/exercise4.2.js
+++ exercises/bloco_4/dia_4/exercise4.2.js
@@ -18,5 +18,40 @@ function palindromo (string) {
 console.log(palindromo("desenvolvimento"));
 console.log(palindromo("arara"));
 
+function retornaIndiceMaiorValor (array){
+  let numeros = array;
+  let maior = numeros[0];
+  let indiceMaior = 0;
+  for (let key in numeros) {
+    if(numeros[key]> maior){
+      maior = numeros[key];
+      indiceMaior = key;
+    }
+  }
+  return indiceMaior
+}
+
+function retornaIndiceMenorValor (array){
+  let numeros = array;
+  let menor = numeros[0];
+  let indiceMenor = 0;
+  for (let key in numeros) {
+    if(numeros[key] < menor){
+      menor = numeros[key];
+      indiceMenor = key;
+    }
+  }
+  return indiceMenor
+}
+
+let numeros = [2, 4, 6, 7, 10, 0, -3];
+
+//Retorna índice com o maior valor
+let indiceMaiorNumero = retornaIndiceMaiorValor(numeros);
+console.log("O índice do maior valor é o "+indiceMaiorNumero);
+
+//Retorna índice com o menor valor
+let indiceMenorNumero = retornaIndiceMenorValor(numeros);
+console.log("O índice do menor valor é o "+indiceMenorNumero);
 
 
