55283d2a653f302fbdfb971f2ef9bd5749992d01
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-06-28 21:50:49 -0300

    Adiciona exercício 7

--- exercises/bloco_4/dia_4/exercise4.2.6.js
+++ exercises/bloco_4/dia_4/exercise4.2.6.js
@@ -1,3 +1,7 @@
+//Exercício 6
+/* Crie uma função que receba um número inteiro N e retorne o 
+somatório de todos os números de 1 até N. */
+
 function soma1aN (n) {
   let soma = 0;
   for(let index=1; index <= n; index +=1) {
