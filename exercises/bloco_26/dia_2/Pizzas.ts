47399ccd3be8791dc390df0dfa84e596b154926f
LucasAccurcio <lucas.accurcio@gmail.com>
2022-03-08 19:43:38 -0300

    resolucao de exercicios  e exemplos

--- exercises/bloco_26/dia_2/Pizzas.ts
+++ exercises/bloco_26/dia_2/Pizzas.ts
@@ -23,3 +23,47 @@ let nutella: Pizza = {
   slice: 4,
 }
 console.log(nutella);
+
+interface CommumPizzas extends Pizza {
+  flavor: 'Calabresa' | 'Frango' | 'Pepperoni',
+}
+
+interface VegPizzas extends Pizza {
+  flavor: 'Marguerita' | 'Palmito' | 'Cogumelo',
+}
+
+type Sugar = 'Nutella' | 'Goiabada com Queijo' | 'Marshmallow';
+interface SweetPizzas extends Pizza {
+  flavor: Sugar,
+  slice: 4,
+}
+
+let calabresa2: CommumPizzas = {
+  flavor: 'Calabresa',
+  slice: 8,
+}
+console.log(calabresa2);
+
+let frango: CommumPizzas = {
+  flavor: 'Frango',
+  slice: 6,
+}
+console.log(frango);
+
+let pepperoni: CommumPizzas = {
+  flavor: 'Pepperoni',
+  slice: 8,
+}
+console.log(pepperoni);
+
+let palmito: VegPizzas = {
+  flavor: 'Palmito',
+  slice: 8,
+}
+console.log(palmito);
+
+let nutella2: SweetPizzas = {
+  flavor: 'Nutella',
+  slice: 4
+};
+console.log(nutella2);
--- exercises/bloco_26/dia_2/dist/Pizzas.js
+++ exercises/bloco_26/dia_2/dist/Pizzas.js
@@ -14,3 +14,28 @@ let nutella = {
     slice: 4,
 };
 console.log(nutella);
+let calabresa2 = {
+    flavor: 'Calabresa',
+    slice: 8,
+};
+console.log(calabresa2);
+let frango = {
+    flavor: 'Frango',
+    slice: 6,
+};
+console.log(frango);
+let pepperoni = {
+    flavor: 'Pepperoni',
+    slice: 8,
+};
+console.log(pepperoni);
+let palmito = {
+    flavor: 'Palmito',
+    slice: 8,
+};
+console.log(palmito);
+let nutella2 = {
+    flavor: 'Nutella',
+    slice: 4
+};
+console.log(nutella2);
