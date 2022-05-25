755514046ffb0ac85bfef2249072425ff42da28f
LucasAccurcio <lucas.accurcio@gmail.com>
2022-05-24 22:31:41 -0300

    feat: :sparkles: implementacação exercicio 1

---
+++ exercises/bloco_33/dia_1/exercises/exercise1.py
@@ -0,0 +1,9 @@
+# Crie uma função que receba dois números e retorne o maior deles.
+
+def return_biggest_number(a, b):
+    if a > b:
+        return a
+    return b
+
+
+print(return_biggest_number(15, 10))
